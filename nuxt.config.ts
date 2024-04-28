9dc74cf929828547d80358e0e9cd9046768e26b7
Ant00000ny <Ant00000ny@icloud.com>
2024-04-28 17:12:58 +0800

    add deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  css: ['~/assets/css/main.css'],
+  modules: ["@nuxt/ui"]
 });
